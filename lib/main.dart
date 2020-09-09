65b39248907a69f4a359c034b56ffe5468d9ac68
leoprananta <leonantapramudya7@gmail.com>
2020-09-09 21:45:03 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_text_speech/screens/bottom_navigation_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavigationScreen()
+    );
+  }
+}
+
